dd435b977383917350ad29d72f7bd11eb62a8421
Stephen Mills <s00132280@mail.itsligo.ie>
2017-09-14 10:50:13 +0100

    Adding Files from array folder
    practice arrays from first year.

---
+++ Arrays/ArraysOne/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysOne
+{
+    class Program
+    {
+        static int[] playerAges = new int[6];
+        static void Main(string[] args)
+        {
+            FillArray();
+            //PrintArray();
+            //PrintArrayFTL();
+            //PrintArrayForEach();
+            //PringReversArray();
+            //double arrayAvarage = GetArrayAvarage();
+            ReverseArray();
+            PrintArray();
+        }
+        // question one
+        static void FillArray()
+        {
+            for (int i = 0; i < playerAges.Length; i++)
+            {
+                Console.Write("Please enter the players age : ");
+                playerAges[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        // question two
+        static void PrintArray()
+        {
+            // printing each element of the array
+            for (int i = 0; i < playerAges.Length; i++)
+            {
+                Console.WriteLine(playerAges[i]);
+            }
+        }
+        // question three
+        static void PrintArrayFTL()
+        {
+            // printing the first, Thired and last element
+            for (int i = 0; i < playerAges.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(playerAges[i]);
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine(playerAges[i]);
+                }
+                else if (i == 5)
+                {
+                    Console.WriteLine(playerAges[i]);
+                }
+            }
+        }
+        // question four
+        static void PrintArrayForEach()
+        {
+            // foreach to print out the elements of an array.
+            foreach (var Age in playerAges)
+            {
+                Console.WriteLine(Age);
+            }
+        }
+        // queston five
+        static void PringReversArray()
+        {
+            for (int i = playerAges.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(playerAges[i]);
+            }
+        }
+        // question six
+        static double GetArrayAvarage()
+        {
+            double total = 0;
+            for (int i = 0; i < playerAges.Length; i++)
+            {
+                total += playerAges[i];
+            }
+            return total / playerAges.Length;
+        }
+        // question seven
+        static bool SearchArray()
+        {
+            bool result = false;
+            int index = 0;
+            Console.Write("Please enter the number you wish to search for : ");
+            int search = int.Parse(Console.ReadLine());
+            // repeats while result is false and we have not reached the end of the array.
+            while (result == false && index < playerAges.Length)
+            {
+                // think of index like i in a for loop.
+                if (playerAges[index] == search)
+                {
+                    result = true;
+                }
+                index++;
+            }
+            return result;
+        }
+        // question eight
+        static void ReverseArray()
+        {
+            Array.Reverse(playerAges);
+        }
+
+    }
+}
---
+++ Arrays/ConsoleApplication1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int[] ArrayInt = {1,2,3,4,5,6,7,8,9};
+        static void Main(string[] args)
+        {
+            //int largest = ChooseLargest(ArrayInt);
+            //Console.WriteLine(largest);
+            //int smallest = ChooseSmallest(ArrayInt);
+            //Console.WriteLine(smallest);
+            /*
+            bool assending = ArraySorted(ArrayInt);
+            if (assending)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            */
+            double avarageOfEveryThiredElement = ArrayThiredAvarage(ArrayInt);
+            Console.WriteLine(avarageOfEveryThiredElement);
+        }
+        static int ChooseLargest(int[] ArrayInt)
+        {
+            int largest = 0;
+            for (int i = 0; i < ArrayInt.Length; i++)
+            {
+                if (ArrayInt[i] > largest)
+                {
+                    largest = ArrayInt[i];
+                }
+            }
+            return largest;
+        }
+        static int ChooseSmallest(int[] ArrayInt)
+        {
+            int smallest = ArrayInt[7];
+            for (int i = 0; i < ArrayInt.Length; i++)
+            {
+                if (ArrayInt[i] < smallest)
+                {
+                    smallest = ArrayInt[i];
+                }
+            }
+            return smallest;
+        }
+        static bool ArraySorted(int[] ArrayInt)
+        {
+            bool assending = true;
+            for (int i = 0; i < ArrayInt.Length-1; i++)
+            {
+                if (ArrayInt[i] > ArrayInt[i+1])
+                {
+                    assending = false;
+                }
+            }
+            return assending;
+        }
+        static double ArrayThiredAvarage(int[] ArrayInt)
+        {
+            double total = 0;
+            int count = 0;
+            for (int i = 0; i < ArrayInt.Length; i+=2)
+            {
+                total += ArrayInt[i];
+                count++;
+            }
+            double avarage = total / count;
+            return total;
+        }
+    }
+}
